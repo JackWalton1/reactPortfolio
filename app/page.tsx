b58d9951705553dc6b3dd1e18a255c268a5962c2
JackWalton1 <jwalto02@calpoly.edu>
2024-03-04 17:54:52 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Welcome! I'm Jack Walton</h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        This is my first full stack website!
+      </p>
+    </section>
+  );
+}
