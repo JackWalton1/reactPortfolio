b7c8723b2c5e8b705d2f4a441330af5b87f714a8
JackWalton1 <jwalto02@calpoly.edu>
2024-04-02 21:21:53 -0700

    fix card wrapping

--- setup.mjs
+++ setup.mjs
@@ -26,6 +26,43 @@ const Box = dynamic(() => import('@mui/material/Box'), { ssr: false });
 
 let myPhoto: string = 'images/avatar.jpg';
 
+export default function Page() {
+  return (
+    <Grid container spacing={2}>
+
+      <Grid item xs={9} alignItems="left">
+        <h1 className="text-left font-medium text-2xl mb-8 tracking-tighter">hey, I'm Jack Walton </h1>
+      </Grid>
+
+      <Grid item xs={3} alignItems="right">
+        <Avatar src={myPhoto} alt="Jack Walton"  sx={{ width: 75, height: 75 }}/>
+      </Grid>
+
+      <Grid item xs={12} >
+        <p className="prose prose-neutral dark:prose-invert">
+        Welcome to my first full stack website! I am a Software Engineering major at Cal Poly, San Luis Obispo. I am interested in many aspects of computing. For example, I enjoy making full stack webapps like this one, creating embedded systems with microcontrollers, and solving complex technical problems in general.
+        </p>
+      </Grid>
+
+      <Grid item xs={12} >
+        <DisplayImages/>
+      </Grid>
+
+      <Grid item xs={12} >
+        <p className="prose prose-neutral dark:prose-invert">
+        I pride myself in my ability to learn new technologies, recognize patterns, and problem solve in any context. I also can explain these complexities to a diverse group of individuals. These are the qualities that make me a great software engineer.
+        </p>
+      </Grid>
+
+      <Grid item xs={12} justifyContent="center">
+        <DisplayActionCards/>
+      </Grid>
+
+    </Grid>
+  );
+}
+
+
 const itemData = [
   {
     img: 'images/microcontrollers.png',
@@ -61,17 +98,16 @@ function DisplayImages(){
 
 const ActionCard = ({ url, avatarSrc, text }) => {
   return (
-    <a href={url}>
+    <a href={url} >
 
-    <Card sx={{ maxHeight: 75 }} style={{ borderRadius: '15px' }} variant="outlined" >
+    <Card sx={{ maxHeight: 75, maxWidth:375 }}  style={{ borderRadius: '15px' }} variant="outlined" >
       <CardActionArea >
         <CardContent className='text-black bg-white dark:text-white dark:bg-[#202020]'>
           <Box
-                    my={-1.5}
-
+          my={-1.5}
           display="flex"
           alignItems="center"
-          justifyContent="space-around"
+          justifyContent="flex-start"
           p={2}
           gap={4}
           component="section"
@@ -90,63 +126,27 @@ const ActionCard = ({ url, avatarSrc, text }) => {
   );
 }
 
-export default function Page() {
-  return (
-    <Grid container spacing={2}>
-
-      <Grid item xs={9} alignItems="left">
-        <h1 className="text-left font-medium text-2xl mb-8 tracking-tighter">hey, I'm Jack Walton </h1>
-      </Grid>
-
-      <Grid item xs={3} alignItems="right">
-        <Avatar src={myPhoto} alt="Jack Walton"  sx={{ width: 75, height: 75 }}/>
-      </Grid>
-
-      <Grid item xs={12} >
-        <p className="prose prose-neutral dark:prose-invert">
-        Welcome to my first full stack website! I am a Software Engineering major at Cal Poly, San Luis Obispo. I am interested in many aspects of computing. For example, I enjoy making full stack webapps like this one, creating embedded systems with microcontrollers, and solving complex technical problems in general.
-        </p>
-      </Grid>
-
-      <Grid item xs={12} >
-        <DisplayImages/>
-      </Grid>
-
-      <Grid item xs={12} >
-        <p className="prose prose-neutral dark:prose-invert">
-        I pride myself in my ability to learn new technologies, recognize patterns, and problem solve in any context. I also can explain these complexities to a diverse group of individuals. These are the qualities that make me a great software engineer.
-        </p>
-      </Grid>
-
-      <Grid item xs={12}>
-      <Box
-          my={-2}
-          display="flex"
-          alignItems="center"
-          justifyContent="space-evenly"
-          p={2}
-          gap={4}
-          component="section"
-          >
+function DisplayActionCards(){
+  return(
+    <Grid container spacing={2} alignItems="center" justifyContent="space-around">
+      <Grid item xs={6.1} md={5} alignItems="center" justifyContent="space-around">
         <ActionCard 
           url="https://github.com/JackWalton1" 
           avatarSrc="https://avatars.githubusercontent.com/u/81585486?v=4"
           text="GitHub"
         />
+      </Grid>
 
+      <Grid item xs={6.1} md={5} alignItems="center" justifyContent="space-around">
         <ActionCard 
           url="https://www.linkedin.com/in/thejackwalton/"      
           avatarSrc= "https://media.licdn.com/dms/image/D5635AQGnXJOIHCRG7g/profile-framedphoto-shrink_800_800/0/1709099403881?e=1712714400&v=beta&t=P-o-5itBT9NeKI4lgpOkRcXh7KGxH9fsHfgl1D9X1MM" 
           text="LinkedIn"
-
         />
-      </Box>
       </Grid>
-
     </Grid>
   );
 }
-
 `;
 
 const workPage = `export default function Page() {
