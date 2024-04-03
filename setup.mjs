e43229febc707038257ae28e23c0939c3cf8d498
JackWalton1 <jwalto02@calpoly.edu>
2024-04-02 19:00:44 -0700

    finished home page

--- setup.mjs
+++ setup.mjs
@@ -47,8 +47,8 @@ function DisplayImages(){
     {itemData.map((item) => (
       <ImageListItem key={item.img}>
         <img
-          srcSet={\`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x\`}
-          src={\`${item.img}?w=248&fit=crop&auto=format\`}
+          srcSet={\`\${item.img}?w=248&fit=crop&auto=format&dpr=2 2x\`}
+          src={\`\${item.img}?w=248&fit=crop&auto=format\`}
           alt={item.title}
           loading="lazy"
           style={{ borderRadius: '20px' }}
