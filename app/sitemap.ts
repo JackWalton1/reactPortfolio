8e29da4c4560ec9641d031e15f8f49feaea0b1ad
JackWalton1 <jackwaltoncmw@gmail.com>
2024-05-14 09:14:57 -0700

    security

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
+  let routes = ['', '/blog', '/guestbook', '/projects', '/work'].map((route) => ({
     url: `https://jackwalton.info${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
